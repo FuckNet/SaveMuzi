a9a913188c2cd7bbf25d96fcf16be5b5aa5bb9bb
Choi ji hun <choijh5414@naver.com>
2015-11-29 22:18:10 +0900

    11-29 22:00 이동 완료

--- src/game/Player.java
+++ src/game/Player.java
@@ -3,6 +3,7 @@ package game;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JLabel;
 
@@ -20,14 +21,18 @@ public class Player extends JLabel{
 	private int cnt;
 	private int life;
 	private boolean inv;
-	private boolean isShooting;
 	private Image[] character;
 	private Image img;
+	
+	private boolean state[];
 	
 	public Player() {
 		initData();
 		setSize(PWIDTH, PHEIGHT);
-		
+		state = new boolean[999];
+		for(int i = 0; i < state.length; i++) {
+			state[i] = false;
+		}
 	}
 	
 	public void initData() {
@@ -41,7 +46,6 @@ public class Player extends JLabel{
 		cnt=0;
 		life=3;
 		inv = false;
-		isShooting = false;
 		character = new Image[9];
 		//이미지 저장
 		for(int i = 0; i < 9; i++) {
@@ -54,11 +58,26 @@ public class Player extends JLabel{
 	
 	@Override
 	public void paintComponent(Graphics g) {
-		System.out.println("hi");
 		g.drawImage(character[imgIndex], 0, 0, PWIDTH, PHEIGHT, null);
 	}
 	
+	public void setState(int index, boolean bool) {
+		state[index] = bool;
+	}
+	
 	public void process() {
+		imgIndex = 2;
+		if(state[KeyEvent.VK_UP] == true)
+			y -= speed * 100;
+		if(state[KeyEvent.VK_DOWN] == true)
+			y += speed * 100;
+		if(state[KeyEvent.VK_RIGHT] == true)
+			x += speed * 100;
+		if(state[KeyEvent.VK_LEFT] == true) {
+			x -= speed * 100;
+			imgIndex = 4;
+		}
+		
 		switch(mode){
 		case 1:
 			x+=200;
@@ -70,38 +89,13 @@ public class Player extends JLabel{
 				imgIndex=0;
 			}
 		case 2:
-			if(degree!=-1&&inv) degree=(degree+180)%360;
-			if(degree>-1) {
-				x-=(speed*Math.sin(Math.toRadians(degree))*100);
-				y-=(speed*Math.cos(Math.toRadians(degree))*100);
-			}
-			if(imgIndex==6) {
-				x-=20;
-//				if(cnt%4==0||shoot){
-//					isShooting=false;
-//					shoot=new Bullet(x+2500, y+1500, 0, 0, RAND(245,265), 8);
-//					bullets.add(shoot);
-//					shoot=new Bullet(x+2500, y+1500, 0, 0, RAND(268,272), 9);
-//					bullets.add(shoot);
-//					shoot=new Bullet(x+2500, y+1500, 0, 0, RAND(275,295), 8);
-//					bullets.add(shoot);
-//				}
-				//8myy+=70;
-			}
-			break;
-		case 3:
-			//keybuff=0;
-			imgIndex=8;
-			if(cnt--==0) {
-				mode=0;
-				cnt=50;
-			}
+			setLocation(x/100, y/100);
 			break;
 		}
 		if(x<2000) x=2000;
-		if(x>62000) x=62000;
+		if(x>102400-7000) x=102400-7000;
 		if(y<3000) y=3000;
-		if(y>45000) y=45000;
+		if(y>72000-7000) y=72000-7000;
 	}
 	
 	public void setDegree(int degree) {
@@ -113,19 +107,19 @@ public class Player extends JLabel{
 	}
 	
 	public void setX(int x) {
-		this.x = x;
+		this.x = x * 100;
 	}
 	
 	public void setY(int y) {
-		this.y = y;
+		this.y = y * 100;
 	}
 	
 	public int getX() {
-		return this.x;
+		return this.x/100;
 	}
 	
 	public int getY() {
-		return this.y;
+		return this.y/100;
 	}
 	
 }
--- src/game/SMThread.java
+++ src/game/SMThread.java
@@ -5,117 +5,148 @@ import java.awt.event.KeyEvent;
 import game.GamePanel.Status;
 
 public class SMThread extends Thread {
-	
-	//private GameScreen gameScreen;
+
+	// private GameScreen gameScreen;
 	private Status status;
 	private GamePanel gamePanel;
-	
+
 	private Player players[];
-	
-	//게임 상태 변수
-	private int cnt;	// 그림을 그리는데 사용하는 int 변수
-	private int gameCnt;	// 게임 도중에 사용되는 이벤트 처리성 int 변수
-	
+
+	// 게임 상태 변수
+	private int cnt; // 그림을 그리는데 사용하는 int 변수
+	private int gameCnt; // 게임 도중에 사용되는 이벤트 처리성 int 변수
+
 	public SMThread() {
 		gameInit();
-		
+
 	}
-	
-//	public void setGameScreen(GameScreen gameScreen) {
-//		this.gameScreen = gameScreen;
-//		
-//	}
-	
+
+	// public void setGameScreen(GameScreen gameScreen) {
+	// this.gameScreen = gameScreen;
+	//
+	// }
+
 	public void setGamePanel(GamePanel gamePanel) {
 		this.gamePanel = gamePanel;
 		players = gamePanel.getPlayer();
 	}
-	
+
 	private void gameInit() {
 		cnt = 0;
 		gameCnt = 0;
 		status = Status.GameStart;
 	}
-	
+
 	public void run() {
-		long preTime;	// 루프 간격을 조절하기 위한 시간 체크 값
-		int delay = 17;	// 루프 딜레이, 1/1000초 단위. > 17/1000초 = 58(프레임/초)
+		long preTime; // 루프 간격을 조절하기 위한 시간 체크 값
+		int delay = 17; // 루프 딜레이, 1/1000초 단위. > 17/1000초 = 58(프레임/초)
 		try {
-			while(true) {
+			while (true) {
 				preTime = System.currentTimeMillis();
-				
+
 				gamePanel.repaint();
 				process();
 				keyProcess();
-				
-				Thread.sleep(100);
-				
-				if(System.currentTimeMillis()-preTime<delay) Thread.sleep(delay-System.currentTimeMillis()+preTime);
-				//게임 루프를 처리하는데 걸린 시간을 체크해서 딜레이값에서 차감하여 딜레이를 일정하게 유지한다.
-				//루프 실행 시간이 딜레이 시간보다 크다면 게임 속도가 느려지게 된다.
 
-				if(status!=Status.GameOver) cnt++;
+				Thread.sleep(100);
+
+				if (System.currentTimeMillis() - preTime < delay)
+					Thread.sleep(delay - System.currentTimeMillis() + preTime);
+				// 게임 루프를 처리하는데 걸린 시간을 체크해서 딜레이값에서 차감하여 딜레이를 일정하게 유지한다.
+				// 루프 실행 시간이 딜레이 시간보다 크다면 게임 속도가 느려지게 된다.
+
+				if (status != Status.GameOver)
+					cnt++;
 			}
-		} catch (Exception e)
-		{
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private void process() {
-		switch(status){
-		case GameStart://스타트
+		switch (status) {
+		case GameStart:// 스타트
 			processPlayer();
-			//if(mymode==2) status=2;
+			// if(mymode==2) status=2;
 			break;
-		case PlayScreen://게임화면
+		case PlayScreen:// 게임화면
 			processPlayer();
-			//process_ENEMY();
-			//process_BULLET();
-			//process_EFFECT();
-			//process_GAMEFLOW();
-			//process_ITEM();
+			// process_ENEMY();
+			// process_BULLET();
+			// process_EFFECT();
+			// process_GAMEFLOW();
+			// process_ITEM();
 			break;
-		case GameOver://게임오버
-			//process_ENEMY();
-			//process_BULLET();
-			//process_GAMEFLOW();
+		case GameOver:// 게임오버
+			// process_ENEMY();
+			// process_BULLET();
+			// process_GAMEFLOW();
 			break;
-		case Pause://일시정지
+		case Pause:// 일시정지
 			break;
 		default:
 			break;
 		}
-		if(status!=Status.GameOver) gameCnt++;
+		if (status != Status.GameOver)
+			gameCnt++;
 	}
-	
+
 	private void processPlayer() {
-		for(Player p : players) {
+		for (Player p : players) {
 			p.process();
 		}
 	}
-	
+
 	private void keyProcess() {
-		if(SMKeyListener.getKeyState(KeyEvent.VK_S)) {
-			players[0].setY(players[0].getY() + 3);
-			players[0].setDegree(180);
-			players[0].setImgIndex(2);
-		}
-		if(SMKeyListener.getKeyState(KeyEvent.VK_W)) {
-			players[0].setY(players[0].getY() - 3);
-			players[0].setDegree(0);
-			players[0].setImgIndex(6);
-		}
-		if(SMKeyListener.getKeyState(KeyEvent.VK_A)) {
-			players[0].setX(players[0].getX() - 3);
-			players[0].setDegree(90);
-			players[0].setImgIndex(6);
-		}
-		if(SMKeyListener.getKeyState(KeyEvent.VK_D)) {
-			players[0].setX(players[0].getX() + 3);
-			players[0].setDegree(270);
-			players[0].setImgIndex(6);
-		}
+//		if (SMKeyListener.getKeyState(KeyEvent.VK_DOWN) && SMKeyListener.getKeyState(KeyEvent.VK_LEFT)) {
+//			players[0].setY(players[0].getY() + 10);
+//			players[0].setX(players[0].getX() - 10);
+//			players[0].setDegree(135);
+//			players[0].setImgIndex(4);
+//		} if (SMKeyListener.getKeyState(KeyEvent.VK_DOWN) && SMKeyListener.getKeyState(KeyEvent.VK_RIGHT)) {
+//			players[0].setY(players[0].getY() + 10);
+//			players[0].setX(players[0].getX() + 10);
+//			players[0].setDegree(225);
+//			players[0].setImgIndex(2);
+//		} if (SMKeyListener.getKeyState(KeyEvent.VK_UP) && SMKeyListener.getKeyState(KeyEvent.VK_LEFT)) {
+//			players[0].setY(players[0].getY() - 10);
+//			players[0].setX(players[0].getX() - 10);
+//			players[0].setDegree(45);
+//			players[0].setImgIndex(4);
+//		} if (SMKeyListener.getKeyState(KeyEvent.VK_UP) && SMKeyListener.getKeyState(KeyEvent.VK_RIGHT)) {
+//			players[0].setY(players[0].getY() - 10);
+//			players[0].setX(players[0].getX() + 10);
+//			players[0].setDegree(315);
+//			players[0].setImgIndex(2);
+//		} else {
+//			if (SMKeyListener.getKeyState(KeyEvent.VK_DOWN)) {
+//				players[0].setY(players[0].getY() + 10);
+//				players[0].setDegree(180);
+//				players[0].setImgIndex(2);
+//			}
+//			if (SMKeyListener.getKeyState(KeyEvent.VK_UP)) {
+//				players[0].setY(players[0].getY() - 10);
+//				players[0].setDegree(0);
+//				players[0].setImgIndex(2);
+//			}
+//			if (SMKeyListener.getKeyState(KeyEvent.VK_LEFT)) {
+//				players[0].setX(players[0].getX() - 10);
+//				players[0].setDegree(90);
+//				players[0].setImgIndex(4);
+//			}
+//			if (SMKeyListener.getKeyState(KeyEvent.VK_RIGHT)) {
+//				players[0].setX(players[0].getX() + 10);
+//				players[0].setDegree(270);
+//				players[0].setImgIndex(2);
+//			}
+//		}
+		
+		players[0].setState(KeyEvent.VK_UP, SMKeyListener.getKeyState(KeyEvent.VK_UP));
+		players[0].setState(KeyEvent.VK_DOWN, SMKeyListener.getKeyState(KeyEvent.VK_DOWN));
+		players[0].setState(KeyEvent.VK_RIGHT, SMKeyListener.getKeyState(KeyEvent.VK_RIGHT));
+		players[0].setState(KeyEvent.VK_LEFT, SMKeyListener.getKeyState(KeyEvent.VK_LEFT));
+		
+		
 	}
-	
+
 }
